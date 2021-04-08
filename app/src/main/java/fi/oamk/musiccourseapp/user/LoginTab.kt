e89b20134f3b961cf45b4f380fb8c29b3e64a941
Loïc Catrou <loic.catrou@hotmail.com>
2021-04-08 13:07:08 +0200

    Login and SignUp input verification

--- app/src/main/java/fi/oamk/musiccourseapp/user/LoginTab.kt
+++ app/src/main/java/fi/oamk/musiccourseapp/user/LoginTab.kt
@@ -1,5 +1,6 @@
 package fi.oamk.musiccourseapp.user
 
+import android.graphics.Color
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.util.Log
@@ -7,9 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast
+import androidx.core.graphics.red
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import fi.oamk.musiccourseapp.R
 import fi.oamk.musiccourseapp.databinding.LoginTabBinding
 
 class LoginTab : Fragment(){
@@ -35,19 +39,21 @@ class LoginTab : Fragment(){
                 var enteredEmail = binding.email.text.toString()
                 var enteredPassword = binding.password.text.toString()
 
-                for(i in 0 until usersFromDatabase.size-1){
+                for(i in 0 until usersFromDatabase.size){
                     val user = usersFromDatabase[i] as HashMap<String, String>
                     var email = user.get("email").toString()
                     var password = user.get("password").toString()
 
                     if(enteredEmail == email && enteredPassword == password){
-                        Log.e("firebase","Working")
+                        //Session lacking
+                        findNavController().navigate(R.id.action_loginFragment_to_postsFragment)
                     }
                 }
             }.addOnFailureListener{
                 Log.e("firebase","Error getting data", it)
             }
-
+            binding.textError.text = "Email or password incorrect"
+            binding.textError.setTextColor(Color.RED)
         }
 
 
--- app/src/main/java/fi/oamk/musiccourseapp/user/SignupTab.kt
+++ app/src/main/java/fi/oamk/musiccourseapp/user/SignupTab.kt
@@ -1,14 +1,17 @@
 package fi.oamk.musiccourseapp.user
 
+import android.graphics.Color
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.util.Log
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import fi.oamk.musiccourseapp.R
 import fi.oamk.musiccourseapp.databinding.SignupTabBinding
 
 class SignupTab : Fragment(){
@@ -31,14 +34,24 @@ class SignupTab : Fragment(){
         binding.signupButton.setOnClickListener{
             database.child("users").get().addOnSuccessListener {
                 var switch = 0
-                if(binding.teacherSwitch.isChecked && binding.studentSwitch.isChecked){
-                    switch = 2
+                if(binding.email.text == null ||  binding.name.text == null ||  binding.password.text == null || (!binding.studentSwitch.isChecked && !binding.teacherSwitch.isChecked)){
+                    binding.textError.text = "Missing one or more informations"
+                    binding.textError.setTextColor(Color.RED)
                 }
-                else if(binding.studentSwitch.isChecked){
-                    switch = 1
+                else if(binding.email.text.toString() != binding.password.text.toString()){
+                    binding.textError.text = "Passwords are not matching"
+                    binding.textError.setTextColor(Color.RED)
                 }
-                var user : User = User(0, binding.email.text.toString(), binding.name.text.toString(), binding.password.text.toString(), "empty", switch)
-                database.child("users").child("1").setValue(user)
+                else {
+                    if (binding.teacherSwitch.isChecked && binding.studentSwitch.isChecked) {
+                        switch = 2
+                    } else if (binding.studentSwitch.isChecked) {
+                        switch = 1
+                    }
+                    var user: User = User(0, binding.email.text.toString(), binding.name.text.toString(), binding.password.text.toString(), "empty", switch)
+                    database.child("users").child("1").setValue(user)
+                    findNavController().navigate(R.id.action_loginFragment_self)
+                }
             }.addOnFailureListener{
                 Log.e("firebase","Error getting data", it)
             }
