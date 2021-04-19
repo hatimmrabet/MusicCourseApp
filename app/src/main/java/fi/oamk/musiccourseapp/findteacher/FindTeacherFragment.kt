6cd9fb1456c9f55d2ec1e0699ced7e532298542f
Alan <dasilva.alan@outlook.com>
2021-04-19 11:57:34 +0300

    dynamic findteacher ui

--- app/src/main/java/fi/oamk/musiccourseapp/findteacher/FindTeacherFragment.kt
+++ app/src/main/java/fi/oamk/musiccourseapp/findteacher/FindTeacherFragment.kt
@@ -1,13 +1,15 @@
 package fi.oamk.musiccourseapp.findteacher
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fi.oamk.musiccourseapp.R
 import fi.oamk.musiccourseapp.databinding.FragmentFindTeacherBinding
 
@@ -16,7 +18,17 @@ class FindTeacherFragment : Fragment() {
     private var _binding: FragmentFindTeacherBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var recyclerView: RecyclerView
+
     private val viewModel: FindTeacherViewModel by viewModels()
+
+    // Keeps track of which LayoutManager is in use for the [RecyclerView]
+    private var isLinearLayoutManager = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +42,60 @@ class FindTeacherFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getUsers()
+        recyclerView = binding.recyclerView
+        chooseLayout()
         viewModel.users.observe(viewLifecycleOwner){
             binding.recyclerView.adapter = ItemAdapter(it, findNavController())
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.find_menu, menu)
+
+        val layoutButton = menu.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+    }
+
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(context, 2)
+        }
+
+    }
+
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_baseline_view_list_24)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_baseline_grid_on_24)
+    }
+
+    /**
+     * Determines how to handle interactions with the selected [MenuItem]
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                // Sets isLinearLayoutManager (a Boolean) to the opposite value
+                isLinearLayoutManager = !isLinearLayoutManager
+                // Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+            // Otherwise, do nothing and use the core event handling
+
+            // when clauses require that all possible paths be accounted for explicitly,
+            // for instance both the true and false cases if the value is a Boolean,
+            // or an else to catch all unhandled cases.
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
