121211fc93e830e22cb9f1aad001fae333690801
IhebChemkhi <ihebchemkhi99@gmail.com>
2021-04-19 20:05:33 +0300

    fixed post bugs

--- app/src/main/java/fi/oamk/musiccourseapp/user/CreatePostFragment.kt
+++ app/src/main/java/fi/oamk/musiccourseapp/user/CreatePostFragment.kt
@@ -2,27 +2,25 @@ package fi.oamk.musiccourseapp.user
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-import android.widget.*
-import androidx.core.view.get
-import fi.oamk.musiccourseapp.R
-import fi.oamk.musiccourseapp.databinding.FragmentCreatePostBinding
-import fi.oamk.musiccourseapp.databinding.FragmentPostsBinding
-import android.widget.ArrayAdapter as ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
+import fi.oamk.musiccourseapp.databinding.FragmentCreatePostBinding
 
 class CreatePostFragment : Fragment() {
     private var _binding: FragmentCreatePostBinding? = null
     private val binding get() = _binding!!
+    private val TAG = "MyActivity"
     private lateinit var database: DatabaseReference
     private lateinit var auth: FirebaseAuth
     private lateinit var title : EditText
@@ -65,8 +63,8 @@ class CreatePostFragment : Fragment() {
         hour6 = binding.checkBox11
         hour7 = binding.checkBox12
         hour8 = binding.checkBox13
-        hour9=binding.checkBox14
-        hour10= binding.checkBox15
+        hour9 = binding.checkBox14
+        hour10 = binding.checkBox15
         hour11 = binding.checkBox16
         hour12 = binding.checkBox17
         submit = binding.button3
@@ -83,6 +81,10 @@ class CreatePostFragment : Fragment() {
             val instrument = instrument.text.toString()
             val price = price.text.toString()
             val date= date.text.toString()
+            if (title == "" || description == "" || instrument == "" || price == "" || date == ""){
+                Toast.makeText(context, "error", Toast.LENGTH_SHORT).show()
+            }
+            else {
             val key = database.child("posts").push().key.toString()
             val key1 = database.child("hours").push().key.toString()
             val key2 = database.child("hours").push().key.toString()
@@ -98,125 +100,135 @@ class CreatePostFragment : Fragment() {
             val key12 = database.child("hours").push().key.toString()
             val user = Firebase.auth.currentUser
 
-            submit.setOnClickListener {
-                database.child("posts").child(key).child("title").setValue(title)
-                database.child("posts").child(key).child("postkey").setValue(key)
-                database.child("posts").child(key).child("description").setValue(description)
-                database.child("posts").child(key).child("instrument").setValue(instrument)
-                database.child("posts").child(key).child("price").setValue(price)
-                database.child("posts").child(key).child("date").setValue(date)
+
+                    database.child("posts").child(key).child("title").setValue(title)
+                    database.child("posts").child(key).child("postkey").setValue(key)
+                    database.child("posts").child(key).child("description").setValue(description)
+                    database.child("posts").child(key).child("instrument").setValue(instrument)
+                    database.child("posts").child(key).child("price").setValue(price)
+                    database.child("posts").child(key).child("date").setValue(date)
+                    user?.let {
+                        val uid = user.uid
+                        database.child("posts").child(key).child("userkey").setValue(uid)
+                    }
+                    if (hour1.isChecked) {
+                        database.child("hours").child(key1).child("start")
+                            .setValue(hour1.text.toString())
+                        database.child("hours").child(key1).child("reserved").setValue("false")
+                        database.child("hours").child(key1).child("hourkey").setValue(key1)
+                        database.child("hours").child(key1).child("postkey").setValue(key)
+                    }
 
 
-                user?.let {
-                    val uid = user.uid
-                    database.child("posts").child(key).child("userkey").setValue(uid)
+
+                    if (hour2.isChecked) {
+                        database.child("hours").child(key2).child("start")
+                            .setValue(hour2.text.toString())
+                        database.child("hours").child(key2).child("reserved").setValue("false")
+                        database.child("hours").child(key2).child("hourkey").setValue(key2)
+                        database.child("hours").child(key2).child("postkey").setValue(key)
+                    }
+
+
+                    if (hour3.isChecked) {
+                        database.child("hours").child(key3).child("start")
+                            .setValue(hour3.text.toString())
+                        database.child("hours").child(key3).child("reserved").setValue("false")
+                        database.child("hours").child(key3).child("hourkey").setValue(key3)
+                        database.child("hours").child(key3).child("postkey").setValue(key)
+                    }
+
+
+                    if (hour4.isChecked) {
+                        database.child("hours").child(key4).child("start")
+                            .setValue(hour4.text.toString())
+                        database.child("hours").child(key4).child("reserved").setValue("false")
+                        database.child("hours").child(key4).child("hourkey").setValue(key4)
+                        database.child("hours").child(key4).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour5.isChecked) {
+                        database.child("hours").child(key5).child("start")
+                            .setValue(hour5.text.toString())
+                        database.child("hours").child(key5).child("reserved").setValue("false")
+                        database.child("hours").child(key5).child("hourkey").setValue(key5)
+                        database.child("hours").child(key5).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour6.isChecked) {
+                        database.child("hours").child(key6).child("start")
+                            .setValue(hour6.text.toString())
+                        database.child("hours").child(key6).child("reserved").setValue("false")
+                        database.child("hours").child(key6).child("hourkey").setValue(key6)
+                        database.child("hours").child(key6).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour7.isChecked) {
+                        database.child("hours").child(key7).child("start")
+                            .setValue(hour7.text.toString())
+                        database.child("hours").child(key7).child("reserved").setValue("false")
+                        database.child("hours").child(key7).child("hourkey").setValue(key7)
+                        database.child("hours").child(key7).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour8.isChecked) {
+                        database.child("hours").child(key8).child("start")
+                            .setValue(hour8.text.toString())
+                        database.child("hours").child(key8).child("reserved").setValue("true")
+                        database.child("hours").child(key8).child("hourkey").setValue(key8)
+                        database.child("hours").child(key8).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour9.isChecked) {
+                        database.child("hours").child(key9).child("start")
+                            .setValue(hour9.text.toString())
+                        database.child("hours").child(key9).child("reserved").setValue("false")
+                        database.child("hours").child(key9).child("hourkey").setValue(key9)
+                        database.child("hours").child(key9).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour10.isChecked) {
+                        database.child("hours").child(key10).child("start")
+                            .setValue(hour10.text.toString())
+                        database.child("hours").child(key10).child("reserved").setValue("true")
+                        database.child("hours").child(key10).child("hourkey").setValue(key10)
+                        database.child("hours").child(key10).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour11.isChecked) {
+                        database.child("hours").child(key11).child("start")
+                            .setValue(hour11.text.toString())
+                        database.child("hours").child(key11).child("reserved").setValue("false")
+                        database.child("hours").child(key11).child("hourkey").setValue(key11)
+                        database.child("hours").child(key11).child("postkey").setValue(key)
+                    }
+
+
+
+                    if (hour12.isChecked) {
+                        database.child("hours").child(key12).child("start")
+                            .setValue(hour12.text.toString())
+                        database.child("hours").child(key12).child("reserved").setValue("false")
+                        database.child("hours").child(key12).child("hourkey").setValue(key12)
+                        database.child("hours").child(key12).child("postkey").setValue(key)
+                    }
                 }
-                if (hour1.isChecked){
-                    database.child("hours").child(key1).child("start").setValue(hour1.text.toString())
-                    database.child("hours").child(key1).child("reserved").setValue("false")
-                    database.child("hours").child(key1).child("hourkey").setValue(key1)
-                    database.child("hours").child(key1).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour2.isChecked){
-                    database.child("hours").child(key2).child("start").setValue(hour2.text.toString())
-                    database.child("hours").child(key2).child("reserved").setValue("false")
-                    database.child("hours").child(key2).child("hourkey").setValue(key2)
-                    database.child("hours").child(key2).child("postkey").setValue(key)
-                }
-
-
-                if(hour3.isChecked){
-                    database.child("hours").child(key3).child("start").setValue(hour3.text.toString())
-                    database.child("hours").child(key3).child("reserved").setValue("false")
-                    database.child("hours").child(key3).child("hourkey").setValue(key3)
-                    database.child("hours").child(key3).child("postkey").setValue(key)
-                }
-
-
-                if(hour4.isChecked){
-                    database.child("hours").child(key4).child("start").setValue(hour4.text.toString())
-                    database.child("hours").child(key4).child("reserved").setValue("false")
-                    database.child("hours").child(key4).child("hourkey").setValue(key4)
-                    database.child("hours").child(key4).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour5.isChecked){
-                    database.child("hours").child(key5).child("start").setValue(hour5.text.toString())
-                    database.child("hours").child(key5).child("reserved").setValue("false")
-                    database.child("hours").child(key5).child("hourkey").setValue(key5)
-                    database.child("hours").child(key5).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour6.isChecked){
-                    database.child("hours").child(key6).child("start").setValue(hour6.text.toString())
-                    database.child("hours").child(key6).child("reserved").setValue("false")
-                    database.child("hours").child(key6).child("hourkey").setValue(key6)
-                    database.child("hours").child(key6).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour7.isChecked){
-                    database.child("hours").child(key7).child("start").setValue(hour7.text.toString())
-                    database.child("hours").child(key7).child("reserved").setValue("false")
-                    database.child("hours").child(key7).child("hourkey").setValue(key7)
-                    database.child("hours").child(key7).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour8.isChecked){
-                    database.child("hours").child(key8).child("start").setValue(hour8.text.toString())
-                    database.child("hours").child(key8).child("reserved").setValue("true")
-                    database.child("hours").child(key8).child("hourkey").setValue(key8)
-                    database.child("hours").child(key8).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour9.isChecked){
-                    database.child("hours").child(key9).child("start").setValue(hour9.text.toString())
-                    database.child("hours").child(key9).child("reserved").setValue("false")
-                    database.child("hours").child(key9).child("hourkey").setValue(key9)
-                    database.child("hours").child(key9).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour10.isChecked){
-                    database.child("hours").child(key10).child("start").setValue(hour10.text.toString())
-                    database.child("hours").child(key10).child("reserved").setValue("true")
-                    database.child("hours").child(key10).child("hourkey").setValue(key10)
-                    database.child("hours").child(key10).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour11.isChecked){
-                    database.child("hours").child(key11).child("start").setValue(hour11.text.toString())
-                    database.child("hours").child(key11).child("reserved").setValue("false")
-                    database.child("hours").child(key11).child("hourkey").setValue(key11)
-                    database.child("hours").child(key11).child("postkey").setValue(key)
-                }
-
-
-
-                if(hour12.isChecked){
-                    database.child("hours").child(key12).child("start").setValue(hour12.text.toString())
-                    database.child("hours").child(key12).child("reserved").setValue("false")
-                    database.child("hours").child(key12).child("hourkey").setValue(key12)
-                    database.child("hours").child(key12).child("postkey").setValue(key)
-                }
-
             }
-        }
+
     }
 
     override fun onDestroyView() {
