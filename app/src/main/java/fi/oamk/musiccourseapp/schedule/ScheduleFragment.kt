22903bd77265e87b57a62a6c1cdbfaabf82b74ff
Alan <dasilva.alan@outlook.com>
2021-04-21 11:44:04 +0300

    added bagdge to Reservation tab

--- app/src/main/java/fi/oamk/musiccourseapp/schedule/ScheduleFragment.kt
+++ app/src/main/java/fi/oamk/musiccourseapp/schedule/ScheduleFragment.kt
@@ -1,19 +1,28 @@
 package fi.oamk.musiccourseapp.schedule
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import com.google.android.material.tabs.TabLayout
 import fi.oamk.musiccourseapp.R
 import fi.oamk.musiccourseapp.databinding.FragmentPostsBinding
 import fi.oamk.musiccourseapp.databinding.FragmentScheduleBinding
+import fi.oamk.musiccourseapp.schedule.reservation.ReservationViewModel
 
 class ScheduleFragment : Fragment() {
 
+    private val TAG = "ScheduleFragment"
+
     private var _binding: FragmentScheduleBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ReservationViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +38,13 @@ class ScheduleFragment : Fragment() {
         val adapter = ViewPageAdapter(childFragmentManager, binding.tabLayout.tabCount)
         binding.viewPager.adapter = adapter
         binding.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
+
+        viewModel.reservations.observe(viewLifecycleOwner) {
+            val badge = binding.tabLayout.getTabAt(1)?.orCreateBadge
+            badge?.number = it.size
+        }
+
+
 
         binding.tabLayout.addOnTabSelectedListener(object :
             TabLayout.OnTabSelectedListener {
