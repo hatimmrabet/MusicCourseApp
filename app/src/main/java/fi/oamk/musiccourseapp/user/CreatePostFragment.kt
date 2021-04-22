29c1774c32a0e9a03f8dafbba4b75f058f1181b9
hatimmrabet <mrabet.hatim2018@gmail.com>
2021-04-22 05:18:25 +0300

    Modify create post UI

--- app/src/main/java/fi/oamk/musiccourseapp/user/CreatePostFragment.kt
+++ app/src/main/java/fi/oamk/musiccourseapp/user/CreatePostFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
@@ -26,7 +27,7 @@ class CreatePostFragment : Fragment() {
     private lateinit var description : EditText
     private lateinit var instrument : EditText
     private lateinit var price : EditText
-    private lateinit var datePicker : TextView
+    private lateinit var datePicker : TextInputLayout
     private lateinit var hour1: CheckBox
     private lateinit var hour2: CheckBox
     private lateinit var hour3: CheckBox
@@ -79,7 +80,7 @@ class CreatePostFragment : Fragment() {
                 {
                     dateDay="0"+dateDay
                 }
-                binding.datePicker.text = "$dateYear/$dateMonth/$dateDay"
+                binding.datePicker.hint = "$dateYear/$dateMonth/$dateDay"
             }, year, month, day)
             tpd.show()
         }
@@ -106,7 +107,7 @@ class CreatePostFragment : Fragment() {
             val description= description.text.toString()
             val instrument = instrument.text.toString()
             val price = price.text.toString()
-            val date= datePicker.text.toString()
+            val date= datePicker.toString()
             if (title == "" || description == "" || instrument == "" || price == "" || date == ""){
                 Toast.makeText(context, "error", Toast.LENGTH_SHORT).show()
             }
