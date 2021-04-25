aa6323edcc9f10cfcd90e07e12457ecb50f33f2c
hatimmrabet <mrabet.hatim2018@gmail.com>
2021-04-25 22:52:17 +0300

    display reservations per day with important info

--- app/src/main/java/fi/oamk/musiccourseapp/schedule/calendar/ItemAdapter.kt
+++ app/src/main/java/fi/oamk/musiccourseapp/schedule/calendar/ItemAdapter.kt
@@ -1,34 +1,41 @@
 package fi.oamk.musiccourseapp.schedule.calendar
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import fi.oamk.musiccourseapp.databinding.ItemDateBinding
+import fi.oamk.musiccourseapp.R
 import fi.oamk.musiccourseapp.schedule.reservation.Date
 
-class ItemAdapter(private val dataset: ArrayList<Date>)
-    : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>()
-{
+class ItemAdapter(private val dataset: ArrayList<ReservationData>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(val binding: ItemDateBinding): RecyclerView.ViewHolder(binding.root)
+    class ItemViewHolder(val binding: View) : RecyclerView.ViewHolder(binding) {
+        val hour: TextView = itemView.findViewById(R.id.hour)
+        val date: TextView = itemView.findViewById(R.id.date)
+        val name: TextView = itemView.findViewById(R.id.name)
+        val email: TextView = itemView.findViewById(R.id.email)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        return ItemViewHolder(
-            ItemDateBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+        val myView = LayoutInflater.from(parent.context).inflate(R.layout.reservation_row,parent,false)
+        dataset.sortByDescending { it.date }
+        return ItemViewHolder(myView)
     }
 
     override fun getItemCount() = dataset.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val date = dataset[position]
-        holder.binding.dateTextView.text = date.start+ " "+date.end+ " "+date.studentId+" "+date.teacherId
+        val data = dataset[position]
+        val start = data.start.substring(0,2)+":"+data.start.substring(2,4)
+        val end = data.end.substring(0,2)+":"+data.end.substring(2,4)
+        val date = data.date.substring(0,4)+"/"+data.date.substring(4,6)+"/"+data.date.substring(6,8)
+
+        holder.date.text = date
+        holder.hour.text = "From "+ start + " to " + end
+        holder.name.text = data.studentName
+        holder.email.text = data.studentEmail
     }
+
+
 }
---
+++ app/src/main/java/fi/oamk/musiccourseapp/schedule/calendar/ReservationData.kt
@@ -0,0 +1,3 @@
+package fi.oamk.musiccourseapp.schedule.calendar
+
+data class ReservationData(val date:String, val start: String, val end:String, val studentName: String?, val studentEmail: String?)
